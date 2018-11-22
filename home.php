8eaec2543489800c4598e716717a3d8cab5dba1e
gn00073 <gihan.kumara@varunlanka.com>
2018-11-22 18:52:39 +0530

    ssss

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=300px, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <table><tr><th><td>Gihan</td><td>amith</td></th></tr><tr><td>ss</td>
+    <td>ss</td>
+    <td>ss</td></tr></table>
+</body>
+</html>
